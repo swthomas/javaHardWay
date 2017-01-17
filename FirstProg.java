05710a1e4fcda1728e47daba3296db2b38863f79
Seth Thomas <swthomas34@gmail.com>
2017-01-17 12:48:49 -0700

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main(String []args) {
+		System.out.println("I, Seth Thomas, am determined to learn how to code.");
+		System.out.println("Today's date is 15 January 2017!");
+	}
+}
