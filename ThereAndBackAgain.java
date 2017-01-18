9e781407c3e1718c1bc1c3f514f05c7b414d9df6
Seth Thomas <swthomas34@gmail.com>
2017-01-18 14:15:43 -0700

    add exercise 33

---
+++ ThereAndBackAgain.java
@@ -0,0 +1,13 @@
+public class ThereAndBackAgain{
+	public static void main(String[]args) {
+	System.out.println("Here.");
+	erebor();
+	System.out.println("Back First Time.");
+	erebor();
+	System.out.println("Back Second time");
+	}
+	
+	public static void erebor() {
+		System.out.println("There.");
+	}
+}
